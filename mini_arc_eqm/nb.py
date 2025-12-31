7470894a2fbb03043b51091bb2774212b8009959
Jack Blackwood <jack.blackwood.caltech@gmail.com>
2025-12-30 21:49:05 -0800

    200_epoch_training

--- mini_arc_eqm/nb.py
+++ mini_arc_eqm/nb.py
@@ -1274,13 +1274,13 @@ def main():
         seq_len=200,
         num_cell_values=10,
         # Denoising evaluation parameters
-        eval_denoise_epoch_interval=1,
-        eval_denoise_gamma=[0.75],
+        eval_denoise_epoch_interval=5,
+        eval_denoise_gamma=[0.0, 0.5, 0.75],
         eval_denoise_mu=0.3,
         eval_denoise_eta=0.003,
         eval_denoise_num_iterations=2000,
         # Training parameters
-        num_epochs=40,
+        num_epochs=200,
         batch_size=128,
         learning_rate=1e-3,
         # Optional: Load existing model to continue training
