0312e6e30213009df05733a1cb304f590771e911
Jack Blackwood <jack.blackwood.caltech@gmail.com>
2025-12-31 12:33:08 -0800

    Update eval denoise logic

--- mini_arc_eqm/nb.py
+++ mini_arc_eqm/nb.py
@@ -1297,8 +1297,8 @@ def main():
         # Data parameters
         seq_len=200,
         # Denoising evaluation parameters
-        eval_denoise_epoch_interval=2,
-        eval_denoise_gamma=[0.0],
+        eval_denoise_epoch_interval=5,
+        eval_denoise_gamma=[0.0, 0.5, 0.8],
         eval_denoise_mu=0.3,
         eval_denoise_eta=0.003,
         eval_denoise_num_iterations=2000,
