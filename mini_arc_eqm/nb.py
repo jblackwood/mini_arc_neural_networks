bb79853cd5ba14bdefff34137dafb829186397c2
Jack Blackwood <jack.blackwood.caltech@gmail.com>
2026-01-15 10:56:32 -0800

    Try l1 weight of 100

--- mini_arc_eqm/nb.py
+++ mini_arc_eqm/nb.py
@@ -1943,7 +1943,7 @@ def main():
         num_layers=8,
         dim_feedforward=1024,
         dropout=0.1,
-        l1_weight=1.0,
+        l1_weight=100,
         # Data parameters
         vocab_size=11,
         # Denoising evaluation parameters
