957f2a4c837aec0e7f3124c528a16d16dde35885
Jack Blackwood <jack.blackwood.caltech@gmail.com>
2026-01-23 09:16:22 -0800

    add result

--- mini_arc_eqm/create_tensor_board_events.py
+++ mini_arc_eqm/create_tensor_board_events.py
@@ -329,7 +329,7 @@ def write_to_tensorboard(metrics, log_dir: Path, model_name: str):
 
 def main():
     # ============ Configure these variables ============
-    md_file = "mini_arc_eqm/results/20260121_232740_model.md"
+    md_file = "mini_arc_eqm/results/20260122_170213_epoch_150_checkpoint.md"
     log_dir = "output/mini_arc_eqm5/runs"
     # ===================================================
     
