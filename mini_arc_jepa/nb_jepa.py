6185c0d73949194f06e7807111369b1996b5957d
Jack Blackwood <jack.blackwood.caltech@gmail.com>
2026-01-23 22:59:17 -0800

    try 0.005

--- mini_arc_jepa/nb_jepa.py
+++ mini_arc_jepa/nb_jepa.py
@@ -1562,7 +1562,7 @@ def main():
         num_epochs=150,
         batch_size=128,
         learning_rate=1e-4,
-        lambd=0.05,
+        lambd=0.005,
         mode="train",
         checkpoint_save_interval=50,
         eval_interval=1,
