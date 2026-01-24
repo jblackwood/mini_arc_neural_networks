383f1cfdb1d2639c871a3f4b6fd294d3386c5b43
Jack Blackwood <jack.blackwood.caltech@gmail.com>
2026-01-23 22:00:27 -0800

    Try smaller lambda

--- mini_arc_jepa/nb_jepa.py
+++ mini_arc_jepa/nb_jepa.py
@@ -1556,7 +1556,7 @@ def main():
         num_epochs=150,
         batch_size=128,
         learning_rate=1e-4,
-        lambd=0.05,
+        lambd=0.00001,
         mode="train",
         checkpoint_save_interval=50,
         eval_interval=1,
