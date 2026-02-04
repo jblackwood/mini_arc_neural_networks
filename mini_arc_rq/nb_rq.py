74076f23cee2ed8567ea25eb93465bf65759b898
Jack Blackwood <jack.blackwood.caltech@gmail.com>
2026-02-04 13:15:05 -0800

    task_codebook_dim of 16

--- mini_arc_rq/nb_rq.py
+++ mini_arc_rq/nb_rq.py
@@ -2091,7 +2091,7 @@ def main():
         dim_feedforward=1024,
         dropout=0.1,
         task_codebook_size=64,
-        task_codebook_dim=8,
+        task_codebook_dim=16,
         num_task_codebooks=16,
         num_task_latent_tokens=10,
         # Data parameters
