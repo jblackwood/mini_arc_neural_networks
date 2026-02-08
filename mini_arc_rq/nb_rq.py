058985aaef58accbc196af59fe58efbf26f3367d
Jack Blackwood <jack.blackwood.caltech@gmail.com>
2026-02-07 20:22:25 -0800

    Go back to smaller codebook and no label smoothing

--- mini_arc_rq/nb_rq.py
+++ mini_arc_rq/nb_rq.py
@@ -2090,9 +2090,9 @@ def main():
         num_layers=8,
         dim_feedforward=1024,
         dropout=0.1,
-        task_codebook_size=256,
+        task_codebook_size=64,
         task_codebook_dim=16,
-        num_task_latent_tokens=25,
+        num_task_latent_tokens=10,
         # Data parameters
         vocab_size=11,
         # Denoising evaluation parameters
@@ -2104,7 +2104,7 @@ def main():
         task_embedding_lr=1e-2,
         weight_decay=0,
         task_embedding_weight_decay=0,
-        label_smoothing=0.1,
+        label_smoothing=0,
         mode="train",
         checkpoint_save_interval=50,
         # Google Drive location for Colab
